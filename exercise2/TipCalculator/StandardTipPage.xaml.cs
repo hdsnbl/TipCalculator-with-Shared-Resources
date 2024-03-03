dc8ada7c55337f6a0cb6887779d3036ab96a667e
hdsnbl <baileyhuds@gmail.com>
2024-03-02 20:59:25 -0600

    Changed the light and dark function to just change the color resources, and then changed all the StaticResources to DynamicResources

--- exercise2/TipCalculator/StandardTipPage.xaml.cs
+++ exercise2/TipCalculator/StandardTipPage.xaml.cs
@@ -27,24 +27,14 @@ public partial class StandardTipPage : ContentPage
 
     void OnLight(object sender, EventArgs e)
     {
-        LayoutRoot.BackgroundColor = colorSilver;
-
-        tipLabel.TextColor = colorNavy;
-        billLabel.TextColor = colorNavy;
-        totalLabel.TextColor = colorNavy;
-        tipOutput.TextColor = colorNavy;
-        totalOutput.TextColor = colorNavy;
+        Resources["fgColor"] = colorNavy;
+        Resources["bgColor"] = colorSilver;
     }
 
     void OnDark(object sender, EventArgs e)
     {
-        LayoutRoot.BackgroundColor = colorNavy;
-
-        tipLabel.TextColor = colorSilver;
-        billLabel.TextColor = colorSilver;
-        totalLabel.TextColor = colorSilver;
-        tipOutput.TextColor = colorSilver;
-        totalOutput.TextColor = colorSilver;
+        Resources["fgColor"] = colorSilver;
+        Resources["bgColor"] = colorNavy;
     }
 
     async void GotoCustom(object sender, EventArgs e)
